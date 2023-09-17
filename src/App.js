6283b081e2484f02c45c31268838a80ed439c1c6
Ritesh Yadav <riteshkumaryadav@gmail.com>
2023-09-17 11:23:20 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+ import './index.css';
+
+
+const  App = () => {
+  const state = useState();
+// console.log(useState);
+const [count, setCount] = useState(0);
+
+const IncNum = () => {
+  setCount(count+1);
+  console.log('clicked'+count);
+};
+const dcrNum = () => {
+  setCount(count-1);
+  console.log('clicked'+count);
+};
+
+      return (
+        <>
+        <h1 className='heading'>{count}</h1>
+        <button onClick= {IncNum} className='btn'>+</button>
+        <button onClick= {dcrNum} className='btn'>-</button>
+        </>
+      )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,95 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// import MissedGoal from './MissedGoal';
+// import MadeGoal from './Made'
+
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ternary operator
+// function Goal(props) {
+//   const isGoal = props.isGoal;
+//   return(
+//     <>
+//      {isGoal ? <MadeGoal /> : <MissedGoal />}
+//      </>
+//   );
+// }
+
+// class Car extends React.Component {
+//   render() {
+//     return <h2>Hi, I am a Car!</h2>;
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Car  />)
+
+// ternary operator
+
+
+//  && operator
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// function Garage(props) {
+//   const cars = props.cars;
+//   return (
+//     <>
+//       <h1>Garage</h1>
+//       {cars.length > 0 &&
+//         <h2>
+//           You have {cars.length} cars in your garage.
+//         </h2>
+//       }
+//     </>
+//   );
+// }
+
+// const cars = ['Ford', 'BMW', 'Audi'];
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Garage cars={cars} />);
+
+
+
+
+//  if else 
+
+
+
+// function Goal(props) {
+//   const isGoal = props.isGoal;
+//     if(isGoal){
+//       return <MadeGoal />
+//     }
+
+//     return <MissedGoal />
+// }
+// ReactDOM.render(
+//   <>
+//   <h1>Conditional statement</h1>
+//   <Goal isGoal={true}/>
+//   </>,
+//   document.getElementById("root")
+// );
